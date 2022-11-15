06a6cf9a34c2d09b7b84ab5f403759ce8ea0ff62
Gabriel-Delazeri <gbldelazeri@gmail.com>
2022-11-15 19:35:08 -0300

    feat: TheMovieDBService

--- app/Services/TheMovieDBService.php
+++ app/Services/TheMovieDBService.php
@@ -6,10 +6,28 @@ use Illuminate\Support\Facades\Http;
 
 class TheMovieDBService
 {
-    public function getPopularMovies()
+    /**
+     * @param string $page
+     * @return array
+     */
+    public function getPopularMovies(string $page = "1") : array
     {
         return Http::themoviedatabase()
-            ->get('/movie/popular?api_key=' . config('tmdb.key') . '&language=en-US&page')
+            ->get('/movie/popular?api_key='.config('tmdb.key').
+                '&language='.config('tmdb.language').
+                '&page='.$page)
+            ->json();
+    }
+
+    /**
+     * @param string $movieId
+     * @return array
+     */
+    public function getMovieInformation(string $movieId) : array
+    {
+        return Http::themoviedatabase()
+            ->get('/movie/'.$movieId.'?api_key='.config('tmdb.key').
+                '&language='.config('tmdb.language'))
             ->json();
     }
 }
